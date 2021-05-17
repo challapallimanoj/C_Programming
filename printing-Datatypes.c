fac795bba824c14c9be552302b9cb27163835262
MANOJ CHALLPALLI <74009435+challapallimanoj@users.noreply.github.com>
2021-05-17 11:05:15 +0530

    Update printing-Datatypes.c

--- printing-Datatypes.c
+++ printing-Datatypes.c
@@ -6,6 +6,13 @@ int main()
   double c;
   char v;
 	scanf("%d%f%lf %c",&a,&b,&c,&v);
-	printf("%d%f%lf%c",a,b,c,v);
+	printf("%d %f %lf %c",a,b,c,v);
 	return 0;
 }
+//sample input:
+// 1
+// 1.2
+// 1.222
+// T
+// sample output:
+// 1 1.2 1.222 T
