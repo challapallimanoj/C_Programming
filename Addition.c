78818026fc7f5d76bc872cd2f87b594bc6f981ce
MANOJ CHALLPALLI <74009435+challapallimanoj@users.noreply.github.com>
2021-05-18 09:05:30 +0530

    addition of 2 numbers
    In this, we have used the addition operator "+".

---
+++ Addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+  {
+  int a,b,sum;
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("addition is %d",sum);
+	return 0;
+}
+// sample input:       sample output:
+//  2 3                  addition is 5
