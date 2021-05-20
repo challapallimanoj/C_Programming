cc67f5b813bce6423ac2ffc7da075e7e63bdc463
MANOJ CHALLPALLI <74009435+challapallimanoj@users.noreply.github.com>
2021-05-20 09:21:22 +0530

    multiplication of intergers
    we have used multiplication operator "*"

---
+++ multiplication of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{	int a,b,mul;
+	scanf("%d%d",&a,&b);
+	mul=a*b;
+	printf("multiplication is %d",mul);
+	return 0;
+}
+//sample output:
+//1 2  
+// multiplication is 2
