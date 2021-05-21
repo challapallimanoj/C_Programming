f31f7c3770ceddde389132062570ba9a1daf860c
MANOJ CHALLPALLI <74009435+challapallimanoj@users.noreply.github.com>
2021-05-21 09:43:40 +0530

    division of two numbers

---
+++ divison of two numbers.c
@@ -0,0 +1,12 @@
+int main()
+{
+	int num1,num2,quotient;
+	printf("Enter two numbers:");
+	scanf("%d %d",&num1,&num2);
+	quotient=num1/num2;
+	printf("Quotient: %d",quotient);
+	return  0;
+}
+// sample output:
+// 2 2   
+// quotient: 1
