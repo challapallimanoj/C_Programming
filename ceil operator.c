fa13871b3d8b9888665746e0b01a24c9c536f6a5
MANOJ CHALLPALLI <74009435+challapallimanoj@users.noreply.github.com>
2021-05-22 09:30:00 +0530

    ceil operator

---
+++ ceil operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  float a,val;
+	scanf("%f",&a);
+	val=ceil(a);
+	printf("nearest grt num is %f",val);
+	return 0;
+}
+//sample output:
+// 1.2
+// nearest grt num is 2.ooooo
